16ac58954dfaaf599630e2c4e210635159ec4042
Guilherme de Lima Conceição <guilimac@localhost.localdomain>
2020-09-27 11:51:38 -0300

    server credentials

---
+++ backend/credentials.js
@@ -0,0 +1,6 @@
+module.exports = {
+  key: null,
+  cert: null,
+  // key: fs.readFileSync('privkey.pem', 'utf8'),
+  // cert: fs.readFileSync('fullchain.pem', 'utf8')
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
+const credentials = require("./credentials");
 const express = require("express");
 const app = express();
-const port = 3001;
-app.listen(port, () => {
+let server;
+if (credentials.key && credentials.cert) {
+  const https = require("https");
+  server = https.createServer(credentials, app);
+  port = 443;
+} else {
+  const http = require("http");
+  server = http.createServer(app);
+  port = 3001;
+}
+
+server.listen(port, () => {
   console.log("server on!");
 });
