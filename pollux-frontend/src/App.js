2f2008c712a73cd4f957a13273ba98d1e1008b19
Guilherme de Lima Conceição <guilimac@localhost.localdomain>
2020-09-27 12:10:03 -0300

    estrutura de colunas

--- pollux-frontend/src/App.js
+++ pollux-frontend/src/App.js
@@ -2,10 +2,24 @@ import React from "react";
 import logo from "./logo.svg";
 import uuid from "react-uuid";
 const textSemderId = uuid();
+import { Container, Row, Col } from "react-bootstrap";
 
 const ENDPOINT = "http://127.0.0.1:3001"; //CHANGE FOR NEW SERVER
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Container>
+        <Row>
+          <Col></Col>
+          <Col></Col>
+        </Row>
+        <Row>
+          <Col></Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
